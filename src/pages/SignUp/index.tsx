caa28200bfc27d7db05f3ecfe005dbbd93c44c91
Abner Willys <abwillys001@gmail.com>
2021-11-13 21:23:03 -0300

    setting navigation

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import { Container } from './styles'
+
+const SignUp = () => {
+  return (
+    <Container>
+      <View />
+    </Container>
+  )
+}
+
+export default SignUp
