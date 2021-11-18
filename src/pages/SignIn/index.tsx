169008c0426c3819e514b66acb787dc00170e460
Abner Willys <abwillys001@gmail.com>
2021-11-18 20:01:39 -0300

    Input and Button dynamic buttons

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Image } from 'react-native'
 
-import { Container, Title } from './styles'
+import Button from '../../components/Button'
+import Input from '../../components/Input'
 
 import logoImg from '../../assets/logo.png'
+
+import { Container, Title } from './styles'
 
 const SignIn = () => {
   return (
@@ -11,6 +14,11 @@ const SignIn = () => {
       <Image source={logoImg} />
 
       <Title>Login</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => console.log('Cliquei')}>Enter</Button>
     </Container>
   )
 }
